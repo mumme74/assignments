9cb167fadc987cc424910b11018a4bb35b1faa0c
Fredrik Johansson <fredrik@mumme.se>
2026-07-30 03:16:03 +0200

    refine UI

--- src/teacher.c
+++ src/teacher.c
@@ -17,14 +17,13 @@
 enum MainPage { FilePage, TestsPage, PersonsPage };
 enum FilePageState { NewFile, OpenFile, SaveFile, ExitFile, SaveFileOk };
 enum TestPageState { AddTest, EditTest, RemoveTest };
-enum PersonPageState { NewPerson, EditPerson, RemovePerson };
+enum PersonPageState { SelectPerson, NewPerson, EditPerson, RemovePerson};
 
 static enum MainPage cur_page = FilePage;
 static enum FilePageState cur_filepage_state = NewFile;
 static enum TestPageState cur_testpage_state = AddTest;
 static enum PersonPageState cur_personpage_state = NewPerson;
 
-static const char *cur_file = NULL; //*cur_dir = NULL;
 static bool contin = true;
 
 static ui_WrapperArr submenus;
@@ -33,9 +32,13 @@ static mem_Arena global_arena, doc_arena;
 static StringArr dir_parts, nav_dir;
 static String filename;
 static Document *doc = NULL;
-static Person cur_user;
+static int cur_person_idx = -1;
 
 static void handle_file_submenu(ui_Window* win, enum FilePageState state);
+static Person* get_person(int idx);
+static void update_person_page_ui(ui_Window *win);
+static void remove_current_person(ui_Window* win);
+static void add_new_person(ui_Window* win);
 
 // ------------------------------------------------------
 
@@ -84,8 +87,20 @@ void evt_test_submenu_click(ui_Wrapper* wrap)
 
 void evt_person_submenu_click(ui_Wrapper* wrap)
 {
-    cur_personpage_state = submenu_btn_nr(wrap);
+    cur_personpage_state = NewPerson + submenu_btn_nr(wrap);
     submenu_set_shown(wrap->parent, false);
+    switch (cur_personpage_state) {
+    case NewPerson:
+        add_new_person(wrap->window);
+        // fallthrough
+    case EditPerson:
+        update_person_page_ui(wrap->window);
+        break;
+    case RemovePerson:
+        remove_current_person(wrap->window);
+        break;
+    default: break;
+    }
 }
 
 void list_dir_in_nav_dir(struct ui_Wrapper* list, String* text, int row)
@@ -112,6 +127,108 @@ void list_dir_in_nav_dir(struct ui_Wrapper* list, String* text, int row)
     }
 
     closedir(dr);
+}
+
+void list_persons(ui_Wrapper* list, String* text, int row)
+{
+    (void)list;
+    if (!doc || row < 0 || doc->persons.size <= (size_t)row)
+        return;
+
+    Person *person = &doc->persons.elements[row];
+    String_append_string(text, &person->name);
+    String_append_str(text, " <", 2);
+    String_append_string(text, &person->email);
+    String_append_str(text, "> ", 2);
+
+    StringArr *roles = Person_roles(person, text->arena);
+    String_append_string(text, StringArr_join(roles, ",", text->arena));
+}
+
+
+void list_user_roles(ui_Wrapper* list, String* text, int row)
+{
+    Person *person = get_person(cur_person_idx);
+    StringArr *roles = StringArr_new(list->window->arena);
+
+    for (int i = 0; i < DOC_ROLES_BYTE_LEN; ++i) {
+        if (person && (person->roles_mask & (0x01 << i)) != 0)
+            StringArr_append(roles, person_role_to_str(0x01 << i), -1);
+    }
+
+    if ((int)roles->size > row)
+        String_set_string(text, &roles->elements[row]);
+}
+
+void list_all_roles(ui_Wrapper* list, String* text, int row)
+{
+    Person *person = get_person(cur_person_idx);
+    StringArr *roles = StringArr_new(list->window->arena);
+
+    for (int i = 0; i < DOC_ROLES_BYTE_LEN; ++i) {
+        if (person && (person->roles_mask & (0x01 << i)) == 0)
+            StringArr_append(roles, person_role_to_str(0x01 << i), -1);
+    }
+
+    if ((int)roles->size > row)
+        String_set_string(text, &roles->elements[row]);
+}
+
+void evt_person_name_changed(ui_Wrapper* edit)
+{
+    Person* person = get_person(cur_person_idx);
+    if (!person) return;
+
+    String_set_string(&person->name, &edit->textedit->text);
+    //ui_control_set_dirty(edit, true);
+}
+
+void evt_person_email_changed(ui_Wrapper* edit)
+{
+    Person* person = get_person(cur_person_idx);
+    if (!person) return;
+
+    String_set_string(&person->email, &edit->textedit->text);
+    //ui_control_set_dirty(edit, true);
+}
+
+void evt_person_roles_add(ui_Wrapper* list, int row)
+{
+    Person* person = get_person(cur_person_idx);
+    if (!person) return;
+
+    String *rolestr = String_new(list->window->arena);
+    list_all_roles(list, rolestr, row);
+    enum PersonRoles role = person_str_to_role(rolestr->elements);
+
+    bool has_changed = person->roles_mask & role;
+    ui_control_set_dirty(list, has_changed);
+
+    list->dirty = person->roles_mask |= role;
+    person->roles_mask |= role;
+
+    ui_Wrapper* roles_ctl = ui_window_get_id(list->window, "PersonRoles");
+    if (roles_ctl)
+        ui_control_set_dirty(roles_ctl, true);
+}
+
+void evt_person_roles_remove(ui_Wrapper* list, int row)
+{
+    Person* person = get_person(cur_person_idx);
+    if (!person) return;
+
+    String *rolestr = String_new(list->window->arena);
+    list_user_roles(list, rolestr, row);
+    enum PersonRoles role = person_str_to_role(rolestr->elements);
+
+    bool has_changed = person->roles_mask & role;
+    ui_control_set_dirty(list, has_changed);
+
+    person->roles_mask &= ~role;
+
+    ui_Wrapper* allroles_ctl = ui_window_get_id(list->window, "PersonAllRoles");
+    if (allroles_ctl)
+        ui_control_set_dirty(allroles_ctl, true);
 }
 
 void evt_go_up_one_dir(ui_Wrapper* wrap)
@@ -148,6 +265,42 @@ void evt_file_list_clicked(ui_Wrapper* list, int row)
             return;
         ui_control_set_text(path, text.elements, text.size);
     }
+}
+
+void evt_person_selected(ui_Wrapper* list, int row)
+{
+    (void)list;
+
+    if (!doc || row < 0 || (size_t)row >= doc->persons.size)
+        return;
+
+    switch (cur_personpage_state) {
+    case SelectPerson:
+    case EditPerson:
+        cur_person_idx = row;
+        // show Edit person view
+        update_person_page_ui(list->window);
+        break;
+    case RemovePerson:
+        cur_person_idx = -1;
+        PersonArr_remove(&doc->persons, row);
+        update_person_page_ui(list->window);
+        break;
+    default:
+        update_person_page_ui(list->window);
+        break;
+    }
+}
+
+void evt_remove_person(ui_Wrapper* btnOk)
+{
+    if (!doc || cur_person_idx < 0 || cur_person_idx >= (int)doc->persons.size)
+        return;
+
+    PersonArr_remove(&doc->persons, cur_person_idx);
+    --cur_person_idx;
+
+    update_person_page_ui(btnOk->window);
 }
 
 void evt_file_ok_overwrite(ui_Wrapper* btnOk)
@@ -273,6 +426,14 @@ static void create_submenus(ui_Window* win)
 // -------------------------------------------------------
 // Page helpers
 
+static Person* get_person(int idx)
+{
+    if (!doc || idx < 0|| (size_t)idx >= doc->persons.size)
+        return NULL;
+
+    return &doc->persons.elements[idx];
+}
+
 static void init_doc(ui_Window* win)
 {
     mem_arena_init(&doc_arena);
@@ -287,6 +448,7 @@ static void init_doc(ui_Window* win)
     }
 
     Document_init(doc, &doc_arena);
+    cur_person_idx = -1;
 }
 
 static void new_doc(ui_Window* win)
@@ -332,7 +494,11 @@ static void load_doc(ui_Window* win, const char* path)
 static void save_doc(ui_Window* win, const char* path)
 {
     FILE *fp = fopen(path, "wb");
-    if (!fp || !Document_write(doc, fp, &cur_user)) {
+
+    Person* person = get_person(cur_person_idx);
+    if (!person) return;
+
+    if (!fp || !Document_write(doc, fp, person)) {
         show_info_dialog(win, "Failed to write", read_error());
         clear_error();
     } else if (read_warning() && read_warning()[0]) {
@@ -404,14 +570,192 @@ static void init_current_dirs()
     String_clear(&filename);
 }
 
+static void add_new_person(ui_Window* win)
+{
+    Person new_person;
+    Person_init(&new_person, doc->persons.arena);
+    cur_person_idx = doc->persons.size;
+    if (!PersonArr_push_back(&doc->persons, new_person))
+        show_info_dialog(win, "Mem error", "Failed to add new person");
+
+    cur_personpage_state = NewPerson;
+}
+
+static void remove_current_person(ui_Window* win)
+{
+    Person* person = get_person(cur_person_idx);
+    if (!person) return;
+
+    show_ok_cancel_dialog(win, "Remove user",
+        "Are You sure you want to remove user?",
+         evt_remove_person, NULL);
+}
+
+static void set_person_form(ui_Window* win)
+{
+    ui_Wrapper *form = ui_window_get_id(win, "PersonEditForm"),
+               *name = ui_window_get_id(win, "PersonNameEdit"),
+               *email = ui_window_get_id(win, "PersonEmailEdit");
+
+    if (!doc || !form || !name || !email)
+        return;
+
+    if (cur_person_idx < 0)
+        add_new_person(win);
+
+    Person *user = &doc->persons.elements[cur_person_idx];
+
+    String_set_string(&name->textedit->text, &user->name);
+    String_set_string(&email->textedit->text, &user->email);
+}
+
+static void enable_person_form(ui_Window* win, bool enable)
+{
+    static const char names[4][20] = {
+        "PersonNameEdit", "PersonEmailEdit",
+        "PersonRoles", "PersonAllRoles"
+    };
+
+    for (size_t i = 0; i < sizeof(names)/sizeof(names[0]); ++i) {
+        ui_Wrapper* ctl = ui_window_get_id(win, names[i]);
+        if (ctl)
+            ui_control_set_enabled(ctl, enable);
+    }
+
+    const char* info_msg = enable ? "" : "Need to add a new user first!";
+    ui_Wrapper* info = ui_window_get_id(win, "PersonFormInfo");
+    if (info)
+        ui_control_set_text(info, info_msg, -1);
+
+}
+
+static void update_person_page_ui(ui_Window *win)
+{
+    if (!doc || cur_page != PersonsPage)
+        return;
+
+    ui_Wrapper *select = ui_window_get_id(win, "PersonSelector"),
+               *form =  ui_window_get_id(win, "PersonEditForm");
+
+    switch (cur_personpage_state) {
+    case EditPerson:
+    case NewPerson:
+        ui_control_set_shown(select, false);
+        ui_control_set_shown(form, true);
+        enable_person_form(win, get_person(cur_person_idx) != NULL);
+        set_person_form(win);
+        break;
+    case RemovePerson:
+    case SelectPerson:
+        ui_control_set_shown(select, true);
+        ui_control_set_shown(form, false);
+        break;
+    }
+}
+
+// --------------------------------------------------------------
+// Page parts
+
+ui_Wrapper* create_person_selector(ui_Window* win, ui_RowSelected selected)
+{
+    int cols, rows, w = 40, h = 15;
+    ui_get_screen_size(&cols, &rows);
+
+    int width = MIN(w, cols-2),
+        height = MIN(h,rows-3);
+
+
+    ui_Wrapper* sel_cont = ui_window_new_control(win, UI_ContainerType);
+    ui_rect_set(&sel_cont->rect, 2,3, width,height);
+    ui_window_append(win, sel_cont);
+    sel_cont->id = "PersonSelector";
+
+    ui_Wrapper *sel_lbl = ui_window_new_control(win, UI_LabelType);
+    ui_control_set_text(sel_lbl, "Select Person", 14);
+    ui_rect_set(&sel_lbl->rect, 1,1, 14,1);
+    ui_control_append(sel_cont, sel_lbl);
+
+    ui_Wrapper *sel_list = ui_window_new_control(win, UI_ListType);
+    ui_rect_set(&sel_list->rect, 1,3, width-2,height-2);
+    sel_list->id = "SelectPersonList";
+    sel_list->list->fetch_row = list_persons;
+    sel_list->list->select_row_evt = selected;
+    ui_control_append(sel_cont, sel_list);
+
+    return sel_cont;
+}
+
+static ui_Wrapper* create_person_form(ui_Window* win)
+{
+    int cols, rows, w = 40, h = 15;
+    ui_get_screen_size(&cols, &rows);
+
+    int width = MIN(w, cols-2),
+        height = MIN(h, rows-3);
+
+    ui_Wrapper *cont = ui_window_new_control(win, UI_ContainerType),
+               *info = ui_window_new_control(win, UI_LabelType),
+               *name = ui_window_new_control(win, UI_TextEditType),
+               *email = ui_window_new_control(win, UI_TextEditType),
+               *lbl_name = ui_window_new_control(win, UI_LabelType),
+               *lbl_email = ui_window_new_control(win, UI_LabelType),
+               *roles = ui_window_new_control(win, UI_ListType),
+               *all_roles = ui_window_new_control(win, UI_ListType);
+
+    if (!cont || !name || !email || !info ||
+        !lbl_email || !lbl_name || !roles ||
+        !all_roles
+    )
+        return NULL;
+
+    ui_rect_set(&cont->rect, 2,3, width,height);
+    ui_window_append(win, cont);
+    cont->id = "PersonEditForm";
+
+    info->label->bg_color = cont->container->bg_color;
+    info->label->fg_color = FrontColors.Red;
+    info->id = "PersonFormInfo";
+    ui_rect_set(&info->rect, 1,0, width-2,0);
+    ui_control_append(cont, info);
+
+    ui_control_set_text(lbl_name, "Name", 4);
+    ui_rect_set(&lbl_name->rect, 1,1, 5,1);
+    ui_control_append(cont, lbl_name);
+
+    name->id = "PersonNameEdit";
+    name->textedit->changed = evt_person_name_changed;
+    ui_rect_set(&name->rect, 7,1, width-3,1);
+    ui_control_append(cont, name);
+
+    ui_control_set_text(lbl_email, "Email", 5);
+    ui_rect_set(&lbl_email->rect, 1,3, 5,3);
+    ui_control_append(cont, lbl_email);
+
+    email->id = "PersonEmailEdit";
+    email->textedit->changed = evt_person_email_changed;
+    ui_rect_set(&email->rect, 7,3, width-3,3);
+    ui_control_append(cont, email);
+
+    roles->id = "PersonRoles";
+    roles->list->fetch_row = list_user_roles;
+    roles->list->select_row_evt = evt_person_roles_remove;
+    ui_rect_set(&roles->rect, 1,5, width/2-1,height-3);
+    ui_control_append(cont, roles);
+
+    all_roles->id = "PersonAllRoles";
+    all_roles->list->fetch_row = list_all_roles;
+    all_roles->list->select_row_evt = evt_person_roles_add;
+    ui_rect_set(&all_roles->rect, width/2+1,5, width-2,height-3);
+    ui_control_append(cont, all_roles);
+
+    return cont;
+}
+
 // -------------------------------------------------------
 // start pages
 
-static void create_file_page(ui_Window* win, Document* doc)
+static void create_file_page(ui_Window* win)
 {
-    (void)doc;
-    (void)win;
-
     int rows, cols;
     ui_get_screen_size(&cols, &rows);
 
@@ -429,7 +773,7 @@ static void create_file_page(ui_Window* win, Document* doc)
     ui_Wrapper* fname = ui_window_new_control(win, UI_TextEditType);
     ui_control_set_position(fname, 15,1);
     ui_control_set_size(fname, ui_control_get_width(file_cont)-17, 1);
-    ui_control_set_text(fname, cur_file, -1);
+    ui_control_set_text(fname, filename.elements, filename.size);
     ui_control_append(file_cont, fname);
     fname->id = "FilenameEdit";
 
@@ -459,22 +803,20 @@ static void create_file_page(ui_Window* win, Document* doc)
     dir_list->id = "FileList";
 }
 
-static void create_persons_page(ui_Window* win, Document* doc)
+static void create_persons_page(ui_Window* win)
 {
-    (void)doc;
-    (void)win;
-    ui_Wrapper* wlbl = ui_window_new_control(win, UI_LabelType);
-    ui_rect_set(&wlbl->rect, 35,10, 45,15);
-    ui_Label *lbl = wlbl->label;
-    String_set(&lbl->text, "Persons", 7);
-    ui_window_append(win, wlbl);
+    ui_Wrapper* sel = create_person_selector(win, evt_person_selected);
+    ui_window_append(win, sel);
+
+    ui_Wrapper* edit = create_person_form(win);
+    ui_window_append(win, edit);
+
+    update_person_page_ui(win);
 }
 
 
-static void create_tests_page(ui_Window* win, Document* doc)
+static void create_tests_page(ui_Window* win)
 {
-    (void)doc;
-    (void)win;
     ui_Wrapper* wlbl = ui_window_new_control(win, UI_LabelType);
     ui_rect_set(&wlbl->rect, 35,10, 45,15);
     ui_Label *lbl = wlbl->label;
@@ -482,20 +824,19 @@ static void create_tests_page(ui_Window* win, Document* doc)
     ui_window_append(win, wlbl);
 }
 
-static void create_page(ui_Window* win, Document* doc)
+static void create_page(ui_Window* win)
 {
-
     create_menu(win);
 
     switch (cur_page) {
     case FilePage:
-        create_file_page(win, doc);
+        create_file_page(win);
         break;
     case PersonsPage:
-        create_persons_page(win, doc);
+        create_persons_page(win);
         break;
     case TestsPage:
-        create_tests_page(win, doc);
+        create_tests_page(win);
         break;
     }
 
@@ -513,7 +854,7 @@ static void pageloop()
 
     ui_WrapperArr_init(&submenus, &page_arena);
 
-    create_page(&win, doc);
+    create_page(&win);
     enum MainPage page = cur_page;
 
     while (cur_page == page) {
@@ -547,7 +888,6 @@ int main(int argc, const char *argv[])
     catch_output(true);
 
     mem_arena_init(&global_arena);
-    Person_init(&cur_user, &global_arena);
     new_doc(NULL);
 
     init_current_dirs();
