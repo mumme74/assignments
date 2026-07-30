c82846af3efe1cc224d31c540e586b8df781692a
Fredrik Johansson <fredrik@mumme.se>
2026-07-30 23:58:27 +0200

    Prevent segfault

--- src/types/typestring.c
+++ src/types/typestring.c
@@ -125,7 +125,8 @@ StringArr *String_split(String* string, const char* key, mem_Arena* arena)
 
 size_t String_utf8_len(String* string)
 {
-    if (!string) return 0;
+    if (!string || string->size == 0)
+        return 0;
 
     size_t len = 0, maxlen = string->size;
     for (const char *p = string->elements; *p != '\0' && maxlen--; ++p) {
